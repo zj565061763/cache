8484dd475a09b8636eaf50326c6e95e9a698e28f
zjun <565061763@qq.com>
2017-08-29 17:25:41 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/cache/MainActivity.java
+++ app/src/main/java/com/fanwe/www/cache/MainActivity.java
@@ -20,17 +20,6 @@ public class MainActivity extends AppCompatActivity
         SDDiskCache.init(this);
         SDDiskCache.setGlobalObjectConverter(new JsonObjectConverter());
 
-        SDDiskCache.open().putInt(key, 100);
-        printInt();
-
-        SDDiskCache.open().putLong(key, 200);
-        printLong();
-
-        SDDiskCache.open().putFloat(key, 100.123f);
-        printFloat();
-
-        SDDiskCache.open().putDouble(key, 200.123345d);
-        printDouble();
 
         SDDiskCache.open().putString(key, "hello");
         printString();
@@ -38,26 +27,6 @@ public class MainActivity extends AppCompatActivity
         TestModel model = new TestModel();
         SDDiskCache.open().putObject(model);
         printObject();
-    }
-
-    private void printInt()
-    {
-        Log.i(TAG, "int:" + SDDiskCache.open().hasInt(key) + "," + SDDiskCache.open().getInt(key, 0));
-    }
-
-    private void printLong()
-    {
-        Log.i(TAG, "long:" + SDDiskCache.open().hasLong(key) + "," + SDDiskCache.open().getLong(key, 0));
-    }
-
-    private void printFloat()
-    {
-        Log.i(TAG, "float:" + SDDiskCache.open().hasFloat(key) + "," + SDDiskCache.open().getFloat(key, 0));
-    }
-
-    private void printDouble()
-    {
-        Log.i(TAG, "double:" + SDDiskCache.open().hasDouble(key) + "," + SDDiskCache.open().getDouble(key, 0));
     }
 
     private void printString()
