a412ca4a551bc02b9723369d166c2eb758349578
zjun <565061763@qq.com>
2017-08-29 17:35:10 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/cache/MainActivity.java
+++ app/src/main/java/com/fanwe/www/cache/MainActivity.java
@@ -2,7 +2,7 @@ package com.fanwe.www.cache;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.TextView;
 
 import com.fanwe.library.cache.SDDiskCache;
 
@@ -10,33 +10,23 @@ public class MainActivity extends AppCompatActivity
 {
     private static final String TAG = "MainActivity";
 
-    private String key = "key";
+    private TextView tv_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_info = (TextView) findViewById(R.id.tv_info);
+
         SDDiskCache.init(this);
         SDDiskCache.setGlobalObjectConverter(new JsonObjectConverter());
 
+        TestModel model = new TestModel(); //创建实体
+        SDDiskCache.open().putObject(model); //保存实体
 
-        SDDiskCache.open().putString(key, "hello");
-        printString();
-
-        TestModel model = new TestModel();
-        SDDiskCache.open().putObject(model);
-        printObject();
-    }
-
-    private void printString()
-    {
-        Log.i(TAG, "string:" + SDDiskCache.open().hasString(key) + "," + SDDiskCache.open().getString(key));
-    }
-
-    private void printObject()
-    {
-        Log.i(TAG, "object:" + SDDiskCache.open().hasObject(TestModel.class) + "," + SDDiskCache.open().getObject(TestModel.class));
+        TestModel modelCached = SDDiskCache.open().getObject(TestModel.class); //查询保存的实体
+        tv_info.setText(String.valueOf(modelCached));
     }
 
     @Override
@@ -44,6 +34,5 @@ public class MainActivity extends AppCompatActivity
     {
         super.onDestroy();
         SDDiskCache.open().delete();
-        SDDiskCache.openCache().delete();
     }
 }
