415dcab066d1028f6a0c83136864eac7a4828d12
zjun <565061763@qq.com>
2017-08-31 23:54:16 +0800

    LongHandler

---
+++ lib/src/main/java/com/fanwe/library/cache/handler/LongHandler.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2017 zhengjun, fanwe (http://www.fanwe.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fanwe.library.cache.handler;
+
+import java.io.File;
+
+/**
+ * Created by zhengjun on 2017/8/31.
+ */
+public class LongHandler extends ObjectHandler<Long>
+{
+    private static final String LONG = "long_";
+
+    private StringHandler mStringHandler;
+
+    public LongHandler(File directory)
+    {
+        super(directory);
+        mStringHandler = new StringHandler(directory);
+    }
+
+    private StringHandler getStringHandler()
+    {
+        return mStringHandler;
+    }
+
+    @Override
+    public String getFileKey(String key)
+    {
+        return getStringHandler().getFileKey(LONG + key);
+    }
+
+    @Override
+    protected boolean onPutObject(String key, Long object, File file)
+    {
+        return getStringHandler().putObject(LONG + key, String.valueOf(object));
+    }
+
+    @Override
+    protected Long onGetObject(String key, File file)
+    {
+        String content = getStringHandler().getObject(LONG + key);
+        return Long.valueOf(content);
+    }
+}
