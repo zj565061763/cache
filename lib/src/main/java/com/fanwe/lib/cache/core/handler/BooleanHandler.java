2ba4dd21d2b6a5127926e4c6585f8bce6e90da4c
zjun <565061763@qq.com>
2018-03-20 17:10:12 +0800

    重构

---
+++ lib/src/main/java/com/fanwe/lib/cache/core/handler/BooleanHandler.java
@@ -0,0 +1,32 @@
+package com.fanwe.lib.cache.core.handler;
+
+import com.fanwe.lib.cache.core.IDiskInfo;
+
+/**
+ * Boolean处理类
+ */
+public class BooleanHandler extends AbstractStringHandler<Boolean>
+{
+    public BooleanHandler(IDiskInfo diskInfo)
+    {
+        super(diskInfo);
+    }
+
+    @Override
+    protected String valueToString(Boolean value)
+    {
+        return String.valueOf(value);
+    }
+
+    @Override
+    protected Boolean stringToValue(String string)
+    {
+        return Boolean.valueOf(string);
+    }
+
+    @Override
+    protected String getKeyPrefix()
+    {
+        return "boolean_";
+    }
+}
