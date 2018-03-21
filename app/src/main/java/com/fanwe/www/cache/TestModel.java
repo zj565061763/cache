b8fe31452e552efc1ffcae7af728adcae45a8fa1
zjun <565061763@qq.com>
2018-03-21 11:39:23 +0800

    demo

--- app/src/main/java/com/fanwe/www/cache/TestModel.java
+++ app/src/main/java/com/fanwe/www/cache/TestModel.java
@@ -21,85 +21,14 @@ public class TestModel implements Serializable
     private boolean valueBoolean = true;
 
     private String valueString = "hello";
-    private Map<String, String> mapString;
+    private Map<String, String> mapString = new HashMap<>();
 
     public TestModel()
     {
-        mapString = new HashMap<>();
         for (int i = 0; i < 10000; i++)
         {
             mapString.put(String.valueOf(i), String.valueOf(i + i));
         }
-    }
-
-    public Map<String, String> getMapString()
-    {
-        return mapString;
-    }
-
-    public void setMapString(Map<String, String> mapString)
-    {
-        this.mapString = mapString;
-    }
-
-    public int getValueInt()
-    {
-        return valueInt;
-    }
-
-    public void setValueInt(int valueInt)
-    {
-        this.valueInt = valueInt;
-    }
-
-    public long getValueLong()
-    {
-        return valueLong;
-    }
-
-    public void setValueLong(long valueLong)
-    {
-        this.valueLong = valueLong;
-    }
-
-    public float getValueFloat()
-    {
-        return valueFloat;
-    }
-
-    public void setValueFloat(float valueFloat)
-    {
-        this.valueFloat = valueFloat;
-    }
-
-    public double getValueDouble()
-    {
-        return valueDouble;
-    }
-
-    public void setValueDouble(double valueDouble)
-    {
-        this.valueDouble = valueDouble;
-    }
-
-    public boolean isValueBoolean()
-    {
-        return valueBoolean;
-    }
-
-    public void setValueBoolean(boolean valueBoolean)
-    {
-        this.valueBoolean = valueBoolean;
-    }
-
-    public String getValueString()
-    {
-        return valueString;
-    }
-
-    public void setValueString(String valueString)
-    {
-        this.valueString = valueString;
     }
 
     @Override
