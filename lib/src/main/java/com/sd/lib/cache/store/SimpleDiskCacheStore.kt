b8883ac720cd4f15ef0b40e84c2b55f1c54d8100
zhengjun <565061763@qq.com>
2022-06-13 19:19:02 +0800

    SimpleDiskCacheStore

--- lib/src/main/java/com/sd/lib/cache/store/SimpleDiskCacheStore.kt
+++ lib/src/main/java/com/sd/lib/cache/store/SimpleDiskCacheStore.kt
@@ -1,55 +1,22 @@
-package com.sd.lib.cache.store;
+package com.sd.lib.cache.store
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.File
 
 /**
  * 文件缓存实现类
  */
-public class SimpleDiskCacheStore extends BaseDiskCacheStore {
-    public SimpleDiskCacheStore(File directory) {
-        super(directory);
-    }
+open class SimpleDiskCacheStore(directory: File) : BaseDiskCacheStore(directory) {
 
-    @Override
-    protected boolean putCacheImpl(String key, byte[] value, File file) throws Exception {
-        OutputStream out = null;
-        try {
-            out = new BufferedOutputStream(new FileOutputStream(file));
-            out.write(value);
-            out.flush();
-            return true;
-        } finally {
-            // TODO 关闭out
+    override fun putCacheImpl(key: String, value: ByteArray, file: File): Boolean {
+        return file.outputStream().buffered().use { outputStream ->
+            outputStream.write(value)
+            true
         }
     }
 
-    @Override
-    protected byte[] getCacheImpl(String key, Class<?> clazz, File file) throws Exception {
-        InputStream in = null;
-        ByteArrayOutputStream baos = null;
-        try {
-            in = new BufferedInputStream(new FileInputStream(file));
-            baos = new ByteArrayOutputStream();
-
-            final byte[] buffer = new byte[1024];
-            while (true) {
-                final int length = in.read(buffer);
-                if (length < 0) {
-                    break;
-                }
-
-                baos.write(buffer, 0, length);
-            }
-            return baos.toByteArray();
-        } finally {
-            // TODO 关闭 baos in
+    override fun getCacheImpl(key: String, clazz: Class<*>, file: File): ByteArray {
+        return file.inputStream().buffered().use { inputStream ->
+            inputStream.readBytes()
         }
     }
 }
