15632d4185bc9659a6508100be38dcf1c0bafedd
zhengjun <565061763@qq.com>
2022-06-14 03:51:30 +0800

    demo

--- app/src/main/java/com/sd/www/cache/App.java
+++ app/src/main/java/com/sd/www/cache/App.java
@@ -3,9 +3,11 @@ package com.sd.www.cache;
 import android.app.Application;
 
 import com.sd.lib.cache.CacheConfig;
+import com.sd.lib.cache.store.limit.LimitCountDiskCacheStore;
 import com.sd.www.cache.converter.GlobalEncryptConverter;
 import com.sd.www.cache.converter.GlobalExceptionHandler;
-import com.sd.www.cache.converter.GsonObjectConverter;
+
+import java.io.File;
 
 public class App extends Application {
     @Override
@@ -15,6 +17,9 @@ public class App extends Application {
         final CacheConfig cacheConfig = new CacheConfig.Builder()
                 // 使用腾讯MMKV自定义的CacheStore，如果不设置，默认使用内部存储目录"/data/包名/files/disk_file"
 //                .setCacheStore(new MMKVCacheStore(this))
+
+                // 设置限制文件数量的CacheStore
+//                .setCacheStore(new LimitCountDiskCacheStore(5, new File(getExternalCacheDir(), "app_disk_cache")))
 
                 // 设置对象转换器，如果不设置，使用内部默认的Gson对象转换器
 //                .setObjectConverter(new GsonObjectConverter())
