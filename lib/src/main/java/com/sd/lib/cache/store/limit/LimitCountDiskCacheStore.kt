595234337b244e1223c4dd649b20e7f6ac68814c
zhengjun <565061763@qq.com>
2022-06-14 03:49:06 +0800

    注释

--- lib/src/main/java/com/sd/lib/cache/store/limit/LimitCountDiskCacheStore.kt
+++ lib/src/main/java/com/sd/lib/cache/store/limit/LimitCountDiskCacheStore.kt
@@ -5,7 +5,11 @@ import java.io.File
 /**
  * 限制数量的磁盘缓存
  */
-class LimitCountDiskCacheStore(maxCount: Int, directory: File) : LimitDiskCacheStore(maxCount, directory) {
+class LimitCountDiskCacheStore(
+    /** 最大文件数量，必须大于0 */
+    maxCount: Int,
+    directory: File,
+) : LimitDiskCacheStore(maxCount, directory) {
     override fun sizeOfCache(file: File): Int {
         return 1
     }
--- lib/src/main/java/com/sd/lib/cache/store/limit/LimitSizeDiskCacheStore.kt
+++ lib/src/main/java/com/sd/lib/cache/store/limit/LimitSizeDiskCacheStore.kt
@@ -5,7 +5,11 @@ import java.io.File
 /**
  * 限制文件大小的磁盘缓存
  */
-class LimitSizeDiskCacheStore(maxSize: Int, directory: File) : LimitDiskCacheStore(maxSize, directory) {
+class LimitSizeDiskCacheStore(
+    /** 最大文件大小，单位（B），必须大于0 */
+    maxSize: Int,
+    directory: File,
+) : LimitDiskCacheStore(maxSize, directory) {
     override fun sizeOfCache(file: File): Int {
         return file.length().toInt()
     }
