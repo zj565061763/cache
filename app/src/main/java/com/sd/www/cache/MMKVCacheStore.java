ec2283e2346bfc69bac1ac08ada9b742a699c9d3
zhengjun <565061763@qq.com>
2022-06-14 01:10:34 +0800

    demo

--- app/src/main/java/com/sd/www/cache/MMKVCacheStore.java
+++ app/src/main/java/com/sd/www/cache/MMKVCacheStore.java
@@ -2,6 +2,8 @@ package com.sd.www.cache;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
+
 import com.sd.lib.cache.Cache;
 import com.sd.lib.cache.CacheInfo;
 import com.tencent.mmkv.MMKV;
@@ -10,31 +12,31 @@ import com.tencent.mmkv.MMKV;
  * 自定义CacheStore
  */
 public class MMKVCacheStore implements Cache.CacheStore {
-    private final MMKV mMMKV;
+    private final MMKV _mmkv;
 
     public MMKVCacheStore(Context context) {
         MMKV.initialize(context);
-        mMMKV = MMKV.defaultMMKV();
+        _mmkv = MMKV.defaultMMKV();
     }
 
     @Override
-    public boolean putCache(String key, byte[] value, CacheInfo info) {
-        return mMMKV.encode(key, value);
+    public boolean putCache(@NonNull String key, @NonNull byte[] value, @NonNull CacheInfo info) {
+        return _mmkv.encode(key, value);
     }
 
     @Override
-    public byte[] getCache(String key, CacheInfo info) {
-        return mMMKV.decodeBytes(key);
+    public byte[] getCache(@NonNull String key, @NonNull CacheInfo info) {
+        return _mmkv.decodeBytes(key);
     }
 
     @Override
-    public boolean removeCache(String key, CacheInfo info) {
-        mMMKV.remove(key);
+    public boolean removeCache(@NonNull String key, @NonNull CacheInfo info) {
+        _mmkv.remove(key);
         return true;
     }
 
     @Override
-    public boolean containsCache(String key, CacheInfo info) {
-        return mMMKV.contains(key);
+    public boolean containsCache(@NonNull String key, @NonNull CacheInfo info) {
+        return _mmkv.contains(key);
     }
 }
