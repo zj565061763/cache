9e4536dff8d9011d78e346d8018c7199d8993ee1
zhengjun <565061763@qq.com>
2022-06-14 01:30:35 +0800

    demo

--- app/src/main/java/com/sd/www/cache/App.java
+++ app/src/main/java/com/sd/www/cache/App.java
@@ -15,12 +15,16 @@ public class App extends Application {
         final CacheConfig cacheConfig = new CacheConfig.Builder()
                 // 使用腾讯MMKV自定义的CacheStore，如果不设置，默认使用内部存储目录"/data/包名/files/disk_file"
 //                .setCacheStore(new MMKVCacheStore(this))
+
                 // 设置对象转换器，如果不设置，使用内部默认的Gson对象转换器
-                .setObjectConverter(new GsonObjectConverter())
+//                .setObjectConverter(new GsonObjectConverter())
+
                 // 设置加解密转换器
                 .setEncryptConverter(new GlobalEncryptConverter())
+
                 // 设置异常监听
                 .setExceptionHandler(new GlobalExceptionHandler())
+
                 // 创建对象
                 .build(this);
 
