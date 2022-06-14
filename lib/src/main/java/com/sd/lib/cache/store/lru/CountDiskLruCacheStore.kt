b7a56d0e8331e6531c51dcc59d3caed85d5130d6
zhengjun <565061763@qq.com>
2022-06-14 08:04:10 +0800

    CountDiskLruCacheStore

--- lib/src/main/java/com/sd/lib/cache/store/lru/CountDiskLruCacheStore.kt
+++ lib/src/main/java/com/sd/lib/cache/store/lru/CountDiskLruCacheStore.kt
@@ -2,10 +2,13 @@ package com.sd.lib.cache.store.lru
 
 import java.io.File
 
+/**
+ * 限制数量的Lru算法磁盘缓存
+ */
 class CountDiskLruCacheStore(
-    maxSize: Int,
+    maxCount: Int,
     directory: File,
-) : DiskLruCacheStore(maxSize, directory) {
+) : DiskLruCacheStore(maxCount, directory) {
     override fun onLruCacheSizeOfEntry(file: File): Int {
         return 1
     }
