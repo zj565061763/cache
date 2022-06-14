3a61ccbba694b71680c7b0c17949f7cb6a5bf230
zhengjun <565061763@qq.com>
2022-06-14 14:09:03 +0800

    demo

--- app/src/main/java/com/sd/www/cache/App.java
+++ app/src/main/java/com/sd/www/cache/App.java
@@ -20,8 +20,12 @@ public class App extends Application {
                 // 使用腾讯MMKV自定义的CacheStore，如果不设置，默认使用内部存储目录"/data/包名/files/disk_file"
 //                .setCacheStore(new MMKVCacheStore(this))
 
+                // Lru算法MMKV自定义的CacheStore
+//                .setCacheStore(new MMKVLruCacheStore(this, 1))
+
                 // 设置限制文件数量的CacheStore
 //                .setCacheStore(new CountDiskLruCacheStore(5, new File(getExternalCacheDir(), "app_disk_cache")))
+
 
                 // 设置限制文件大小的CacheStore
 //                .setCacheStore(new SizeDiskLruCacheStore(10 * 1024 * 1024, new File(getExternalCacheDir(), "app_disk_cache")))
