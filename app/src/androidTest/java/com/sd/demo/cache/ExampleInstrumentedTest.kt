9ad5660d7f25d45070f33cd4e6c099748f0cb62f
zhengjun <565061763@qq.com>
2022-06-30 23:58:34 +0800

    testCacheInt

--- app/src/androidTest/java/com/sd/demo/cache/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sd/demo/cache/ExampleInstrumentedTest.kt
@@ -1,6 +1,9 @@
 package com.sd.demo.cache
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.sd.lib.cache.Cache
+import com.sd.lib.cache.FCache
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,6 +16,18 @@ import org.junit.runner.RunWith
 class ExampleInstrumentedTest {
     @Test
     fun testCacheInt() {
+        val key = "TestKey"
+        Assert.assertEquals(Int.MIN_VALUE, FCache.disk().cacheInteger().get(key, Int.MIN_VALUE))
+
+        Assert.assertEquals(true, FCache.disk().cacheInteger().put(key, 1))
+        Assert.assertEquals(1, FCache.disk().cacheInteger().get(key, Int.MIN_VALUE))
+
+        Assert.assertEquals(true, FCache.disk().cacheInteger().remove(key))
+        Assert.assertEquals(Int.MIN_VALUE, FCache.disk().cacheInteger().get(key, Int.MIN_VALUE))
+    }
+
+    private fun <T> testCommonCache(cache: Cache.CommonCache<T>) {
+        // test put
 
     }
 }
