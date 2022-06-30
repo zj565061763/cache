b88919ee225d7e1dcc50c01534132d7ac12a5a7c
zhengjun <565061763@qq.com>
2022-07-01 00:02:44 +0800

    test

--- app/src/androidTest/java/com/sd/demo/cache/TestCommonCache.kt
+++ app/src/androidTest/java/com/sd/demo/cache/TestCommonCache.kt
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class TestCommonCache {
     @Test
     fun testCacheInt() {
         val key = "TestKey"
