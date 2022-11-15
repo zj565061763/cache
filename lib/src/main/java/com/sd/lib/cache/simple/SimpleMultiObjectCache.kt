a579845c9942da68c2f87f7e9085a1cd9a7dbf4a
565061763 <565061763@qq.com>
2022-11-16 00:19:06 +0800

    format

--- lib/src/main/java/com/sd/lib/cache/simple/SimpleMultiObjectCache.kt
+++ lib/src/main/java/com/sd/lib/cache/simple/SimpleMultiObjectCache.kt
@@ -11,27 +11,32 @@ internal class SimpleMultiObjectCache<T>(
     private val _objectHandler = ObjectHandler(cacheInfo, "multi_object")
 
     private fun transformKey(key: String): String {
+        require(key.isNotEmpty()) { "key is empty" }
         return "${objectClass.name}_$key"
     }
 
     override fun put(key: String, value: T?): Boolean {
         if (key.isEmpty()) return false
-        return _objectHandler.putCache(transformKey(key), value)
+        val key = transformKey(key)
+        return _objectHandler.putCache(key, value)
     }
 
     override fun get(key: String): T? {
         if (key.isEmpty()) return null
-        val cache = _objectHandler.getCache(transformKey(key), objectClass) ?: return null
+        val key = transformKey(key)
+        val cache = _objectHandler.getCache(key, objectClass) ?: return null
         return cache as T
     }
 
     override fun remove(key: String): Boolean {
         if (key.isEmpty()) return false
-        return _objectHandler.removeCache(transformKey(key))
+        val key = transformKey(key)
+        return _objectHandler.removeCache(key)
     }
 
     override fun contains(key: String): Boolean {
         if (key.isEmpty()) return false
-        return _objectHandler.containsCache(transformKey(key))
+        val key = transformKey(key)
+        return _objectHandler.containsCache(key)
     }
 }
