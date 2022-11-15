ad976434dc7354b1e4cd9bc3d5c871e7c795c71e
565061763 <565061763@qq.com>
2022-11-16 00:56:19 +0800

    demo

--- app/src/main/java/com/sd/demo/cache/App.java
+++ app/src/main/java/com/sd/demo/cache/App.java
@@ -15,9 +15,7 @@ public class App extends Application {
 
         final CacheConfig cacheConfig = new CacheConfig.Builder()
 
-                // 使用腾讯MMKV自定义的CacheStore，如果不设置，默认使用内部存储目录"/data/包名/files/f_disk_cache"
-//                .setCacheStore(new MMKVCacheStore(this))
-
+                // 设置异常监听
                 .setExceptionHandler(new Cache.ExceptionHandler() {
                     @Override
                     public void onException(@NonNull Exception e) {
