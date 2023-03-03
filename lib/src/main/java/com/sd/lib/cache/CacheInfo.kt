e92ffb9a0aba66e6ecf0aa1d19c26139b99c6283
zj565061763 <565061763@qq.com>
2023-03-03 20:05:49 +0800

    comment

--- lib/src/main/java/com/sd/lib/cache/CacheInfo.kt
+++ lib/src/main/java/com/sd/lib/cache/CacheInfo.kt
@@ -1,12 +1,12 @@
 package com.sd.lib.cache
 
 interface CacheInfo {
-    /** 存取对象 */
+    /** 存取库 */
     val cacheStore: Cache.CacheStore
 
-    /** 对象转换器 */
+    /** 对象转换 */
     val objectConverter: Cache.ObjectConverter
 
-    /** 异常处理对象 */
+    /** 异常处理 */
     val exceptionHandler: Cache.ExceptionHandler
 }
