d8954d8b5a26e9e9193006f79b4d0d1e28e0dd87
zj565061763 <565061763@qq.com>
2023-03-03 19:38:58 +0800

    kotlin

--- app/src/main/java/com/sd/demo/cache/MMKVCacheStore.kt
+++ app/src/main/java/com/sd/demo/cache/MMKVCacheStore.kt
@@ -1,43 +1,36 @@
-package com.sd.demo.cache;
+package com.sd.demo.cache
 
-import android.content.Context;
-
-import androidx.annotation.NonNull;
-
-import com.sd.lib.cache.Cache;
-import com.tencent.mmkv.MMKV;
+import android.content.Context
+import com.sd.lib.cache.Cache.CacheStore
+import com.tencent.mmkv.MMKV
 
 /**
  * 自定义CacheStore。
- * <p>
- * MMKV有个bug，如果存的是空byte[]数组，取的时候会返回null，正常返回应该也是个空的byte[]数组。
+ *
+ * MMKV有个bug，如果存的是空byte[]，取的时候会返回null，正常返回应该也是个空byte[]。
  */
-public class MMKVCacheStore implements Cache.CacheStore {
-    private final MMKV _mmkv;
+class MMKVCacheStore(context: Context?) : CacheStore {
+    private val _mmkv: MMKV
 
-    public MMKVCacheStore(Context context) {
-        MMKV.initialize(context);
-        _mmkv = MMKV.defaultMMKV();
+    init {
+        MMKV.initialize(context)
+        _mmkv = MMKV.defaultMMKV()
     }
 
-    @Override
-    public boolean putCache(@NonNull String key, @NonNull byte[] value) {
-        return _mmkv.encode(key, value);
+    override fun putCache(key: String, value: ByteArray): Boolean {
+        return _mmkv.encode(key, value)
     }
 
-    @Override
-    public byte[] getCache(@NonNull String key) {
-        return _mmkv.decodeBytes(key);
+    override fun getCache(key: String): ByteArray? {
+        return _mmkv.decodeBytes(key)
     }
 
-    @Override
-    public boolean removeCache(@NonNull String key) {
-        _mmkv.remove(key);
-        return true;
+    override fun removeCache(key: String): Boolean {
+        _mmkv.remove(key)
+        return true
     }
 
-    @Override
-    public boolean containsCache(@NonNull String key) {
-        return _mmkv.contains(key);
+    override fun containsCache(key: String): Boolean {
+        return _mmkv.contains(key)
     }
 }
