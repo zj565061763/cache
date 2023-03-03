268d4b41bb48c9251bc629ecf2022d3ce3901013
zj565061763 <565061763@qq.com>
2023-03-03 19:41:42 +0800

    kotlin

--- app/src/main/java/com/sd/demo/cache/App.kt
+++ app/src/main/java/com/sd/demo/cache/App.kt
@@ -1,32 +1,16 @@
-package com.sd.demo.cache;
+package com.sd.demo.cache
 
-import android.app.Application;
-import android.util.Log;
+import android.app.Application
+import android.util.Log
+import com.sd.lib.cache.CacheConfig
 
-import androidx.annotation.NonNull;
-
-import com.sd.lib.cache.Cache;
-import com.sd.lib.cache.CacheConfig;
-
-public class App extends Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        final CacheConfig cacheConfig = new CacheConfig.Builder()
-
-                // 设置异常监听
-                .setExceptionHandler(new Cache.ExceptionHandler() {
-                    @Override
-                    public void onException(@NonNull Exception e) {
-                        Log.e(MainActivity.TAG, String.valueOf(e));
-                    }
-                })
-
-                // 创建对象
-                .build(this);
-
-        // 初始化
-        CacheConfig.init(cacheConfig);
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CacheConfig.init(
+            CacheConfig.Builder()
+                .setExceptionHandler { Log.e(MainActivity.TAG, it.toString()) }
+                .build(this)
+        )
     }
 }
