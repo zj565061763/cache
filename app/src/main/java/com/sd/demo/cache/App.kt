8932f328534d08240bbc7b73980d6e030b236c72
zj565061763 <565061763@qq.com>
2023-03-03 20:00:58 +0800

    demo

--- app/src/main/java/com/sd/demo/cache/App.kt
+++ app/src/main/java/com/sd/demo/cache/App.kt
@@ -1,16 +1,16 @@
 package com.sd.demo.cache
 
 import android.app.Application
-import android.util.Log
 import com.sd.lib.cache.CacheConfig
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        CacheConfig.init(
-            CacheConfig.Builder()
-                .setExceptionHandler { Log.e(MainActivity.TAG, it.toString()) }
-                .build(this)
-        )
+
+        val config = CacheConfig.Builder()
+            .setExceptionHandler { logMsg { "error:$it" } }
+            .build(this)
+
+        CacheConfig.init(config)
     }
 }
--- app/src/main/java/com/sd/demo/cache/MainActivity.kt
+++ app/src/main/java/com/sd/demo/cache/MainActivity.kt
@@ -1,90 +1,75 @@
-package com.sd.demo.cache;
+package com.sd.demo.cache
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.sd.lib.cache.Cache
+import com.sd.lib.cache.CacheConfig
+import com.sd.lib.cache.FCache
 
-import androidx.appcompat.app.AppCompatActivity;
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val key = "key"
+    private val model = TestModel()
 
-import com.sd.lib.cache.Cache;
-import com.sd.lib.cache.CacheConfig;
-import com.sd.lib.cache.FCache;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    public static final String TAG = MainActivity.class.getSimpleName();
-
-    private static final String KEY = "key";
-    private static final TestModel TEST_MODEL = new TestModel();
-
-    private Cache mCache;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 
-    private Cache getCache() {
-        if (mCache == null) {
-            /**
-             * 使用本地磁盘缓存，
-             * 默认使用内部存储目录"/data/包名/files/f_disk_cache"，可以在初始化的时候设置{@link CacheConfig.Builder#setCacheStore(Cache.CacheStore)}
-             */
-            mCache = FCache.disk();
-        }
-        return mCache;
+    private val cache: Cache by lazy {
+        /**
+         * 默认使用内部存储目录"/data/包名/files/f_disk_cache"，可以在初始化的时候设置[CacheConfig.Builder.setCacheStore]
+         */
+        FCache.disk()
     }
 
-    private void putData() {
-        getCache().cacheInteger().put(KEY, 1);
-        getCache().cacheLong().put(KEY, 22L);
-        getCache().cacheFloat().put(KEY, 333.333F);
-        getCache().cacheDouble().put(KEY, 4444.4444D);
-        getCache().cacheBoolean().put(KEY, true);
-        getCache().cacheString().put(KEY, "hello String");
-        getCache().cacheObject().put(TEST_MODEL);
-        getCache().cacheMultiObject(TestModel.class).put(KEY, TEST_MODEL);
-        getCache().cacheMultiObject(TestModel.class).put(KEY + KEY, TEST_MODEL);
+    private fun putData() {
+        cache.cacheInteger().put(key, 1)
+        cache.cacheLong().put(key, 22L)
+        cache.cacheFloat().put(key, 333.333f)
+        cache.cacheDouble().put(key, 4444.4444)
+        cache.cacheBoolean().put(key, true)
+        cache.cacheString().put(key, "hello String")
+        cache.cacheObject().put(model)
+        cache.cacheMultiObject(TestModel::class.java).put(key, model)
+        cache.cacheMultiObject(TestModel::class.java).put(key + key, model)
     }
 
-    private void getData() {
-        Log.i(TAG, "cacheInteger:" + getCache().cacheInteger().get(KEY, 0));
-        Log.i(TAG, "cacheLong:" + getCache().cacheLong().get(KEY, 0L));
-        Log.i(TAG, "cacheFloat:" + getCache().cacheFloat().get(KEY, 0F));
-        Log.i(TAG, "cacheDouble:" + getCache().cacheDouble().get(KEY, 0D));
-        Log.i(TAG, "cacheBoolean:" + getCache().cacheBoolean().get(KEY, false));
-        Log.i(TAG, "cacheString:" + getCache().cacheString().get(KEY, null));
-        Log.i(TAG, "cacheObject:" + getCache().cacheObject().get(TestModel.class));
-        Log.i(TAG, "cacheMultiObject:" + getCache().cacheMultiObject(TestModel.class).get(KEY));
-        Log.i(TAG, "cacheMultiObject:" + getCache().cacheMultiObject(TestModel.class).get(KEY + KEY));
+    private fun getData() {
+        logMsg { "cacheInteger:" + cache.cacheInteger().get(key, 0) }
+        logMsg { "cacheLong:" + cache.cacheLong().get(key, 0L) }
+        logMsg { "cacheFloat:" + cache.cacheFloat().get(key, 0f) }
+        logMsg { "cacheDouble:" + cache.cacheDouble().get(key, 0.0) }
+        logMsg { "cacheBoolean:" + cache.cacheBoolean().get(key, false) }
+        logMsg { "cacheString:" + cache.cacheString().get(key, "") }
+        logMsg { "cacheObject:" + cache.cacheObject().get(TestModel::class.java) }
+        logMsg { "cacheMultiObject:" + cache.cacheMultiObject(TestModel::class.java).get(key) }
+        logMsg { "cacheMultiObject:" + cache.cacheMultiObject(TestModel::class.java).get(key + key) }
     }
 
-    private void removeData() {
-        getCache().cacheInteger().remove(KEY);
-        getCache().cacheLong().remove(KEY);
-        getCache().cacheFloat().remove(KEY);
-        getCache().cacheDouble().remove(KEY);
-        getCache().cacheBoolean().remove(KEY);
-        getCache().cacheString().remove(KEY);
-        getCache().cacheObject().remove(TestModel.class);
-        getCache().cacheMultiObject(TestModel.class).remove(KEY);
-        getCache().cacheMultiObject(TestModel.class).remove(KEY + KEY);
+    private fun removeData() {
+        cache.cacheInteger().remove(key)
+        cache.cacheLong().remove(key)
+        cache.cacheFloat().remove(key)
+        cache.cacheDouble().remove(key)
+        cache.cacheBoolean().remove(key)
+        cache.cacheString().remove(key)
+        cache.cacheObject().remove(TestModel::class.java)
+        cache.cacheMultiObject(TestModel::class.java).remove(key)
+        cache.cacheMultiObject(TestModel::class.java).remove(key + key)
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_put:
-                putData();
-                break;
-            case R.id.btn_get:
-                getData();
-                break;
-            case R.id.btn_remove:
-                removeData();
-                break;
-            default:
-                break;
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_put -> putData()
+            R.id.btn_get -> getData()
+            R.id.btn_remove -> removeData()
+            else -> {}
         }
     }
+}
+
+inline fun logMsg(block: () -> String) {
+    Log.i("cache-demo", block())
 }
