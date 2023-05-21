ac17a0a228daf1254b4359ffb91a95bd773f52a3
zj565061763 <565061763@qq.com>
2023-05-21 12:07:09 +0800

    1.3.6

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "cache"
-val libVersionName = "1.3.5"
+val libVersionName = "1.3.6"
 
 android {
     namespace ="com.sd.lib.cache"
