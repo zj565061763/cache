f3373db6cbbd03ed3c362b92b629b01ef893b2d1
zjun <565061763@qq.com>
2023-12-10 02:16:26 +0800

    MoshiObjectConverter

---
+++ app/src/main/java/com/sd/demo/cache/MoshiObjectConverter.kt
@@ -0,0 +1,21 @@
+package com.sd.demo.cache
+
+import com.sd.lib.cache.Cache
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+class MoshiObjectConverter : Cache.ObjectConverter {
+    private val _moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    override fun <T> encode(value: T, clazz: Class<T>): ByteArray {
+        val adapter = _moshi.adapter(clazz)
+        val json = adapter.toJson(value)
+        return json.toByteArray()
+    }
+
+    override fun <T> decode(bytes: ByteArray, clazz: Class<T>): T {
+        val adapter = _moshi.adapter(clazz)
+        val json = bytes.decodeToString()
+        return adapter.fromJson(json)!!
+    }
+}
