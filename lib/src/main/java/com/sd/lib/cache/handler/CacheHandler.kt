2ec43deb1a859a7fc16f92761693ae342ba3dee3
zjun <565061763@qq.com>
2023-12-10 00:49:20 +0800

    refactor

--- lib/src/main/java/com/sd/lib/cache/handler/CacheHandler.kt
+++ lib/src/main/java/com/sd/lib/cache/handler/CacheHandler.kt
@@ -1,5 +1,10 @@
 package com.sd.lib.cache.handler
 
+import com.sd.lib.cache.Cache
+import com.sd.lib.cache.CacheException
+import com.sd.lib.cache.CacheInfo
+import kotlin.coroutines.cancellation.CancellationException
+
 /**
  * 缓存处理接口
  */
@@ -11,4 +16,118 @@ internal interface CacheHandler<T> {
     fun removeCache(key: String)
 
     fun containsCache(key: String): Boolean
+}
+
+/**
+ * 缓存处理基类
+ */
+internal abstract class BaseCacheHandler<T>(
+    val cacheInfo: CacheInfo,
+    val keyPrefix: String,
+) : CacheHandler<T>, Cache.CommonCache<T> {
+
+    private val _cacheStore: Cache.CacheStore
+        get() = cacheInfo.cacheStore
+
+    init {
+        require(keyPrefix.isNotEmpty()) { "keyPrefix is empty" }
+    }
+
+    //---------- CommonCache start ----------
+
+    final override fun put(key: String, value: T?): Boolean {
+        if (value == null) return false
+        return putCache(key, value, null)
+    }
+
+    final override fun get(key: String): T? {
+        return getCache(key, null)
+    }
+
+    final override fun remove(key: String) {
+        removeCache(key)
+    }
+
+    final override fun contains(key: String): Boolean {
+        return containsCache(key)
+    }
+
+    //---------- CommonCache end ----------
+
+    private fun transformKey(key: String): String {
+        require(key.isNotEmpty()) { "key is empty" }
+        return keyPrefix + "_" + key
+    }
+
+    private fun notifyException(e: Exception) {
+        if (e is CacheException) throw e
+        if (e is CancellationException) throw e
+        cacheInfo.exceptionHandler.onException(e)
+    }
+
+    //---------- CacheHandler start ----------
+
+    final override fun putCache(key: String, value: T, clazz: Class<*>?): Boolean {
+        val key = transformKey(key)
+        synchronized(Cache::class.java) {
+            return try {
+                val data = encodeToByteImpl(value, clazz)
+                _cacheStore.putCache(key, data)
+            } catch (e: Exception) {
+                notifyException(e)
+                false
+            }
+        }
+    }
+
+    final override fun getCache(key: String, clazz: Class<*>?): T? {
+        val key = transformKey(key)
+        synchronized(Cache::class.java) {
+            return try {
+                _cacheStore.getCache(key)?.let { data ->
+                    decodeFromByteImpl(data, clazz)
+                }
+            } catch (e: Exception) {
+                notifyException(e)
+                null
+            }
+        }
+    }
+
+    final override fun removeCache(key: String) {
+        val key = transformKey(key)
+        synchronized(Cache::class.java) {
+            try {
+                _cacheStore.removeCache(key)
+            } catch (e: Exception) {
+                notifyException(e)
+            }
+        }
+    }
+
+    final override fun containsCache(key: String): Boolean {
+        val key = transformKey(key)
+        synchronized(Cache::class.java) {
+            return try {
+                _cacheStore.containsCache(key)
+            } catch (e: Exception) {
+                notifyException(e)
+                false
+            }
+        }
+    }
+
+    //---------- CacheHandler end ----------
+
+    /**
+     * 缓存转byte
+     */
+    @Throws(Exception::class)
+    protected abstract fun encodeToByteImpl(value: T, clazz: Class<*>?): ByteArray
+
+    /**
+     * byte转缓存
+     */
+    @Throws(Exception::class)
+    protected abstract fun decodeFromByteImpl(bytes: ByteArray, clazz: Class<*>?): T
 }
