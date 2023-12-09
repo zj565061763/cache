c32e115cab33bea70a9123463315612845f8ab3e
zjun <565061763@qq.com>
2023-12-10 02:38:12 +0800

    check empty bytes

--- lib/src/main/java/com/sd/lib/cache/handler/ObjectHandler.kt
+++ lib/src/main/java/com/sd/lib/cache/handler/ObjectHandler.kt
@@ -7,9 +7,12 @@ import com.sd.lib.cache.CacheInfo
  * Object处理类
  */
 internal class ObjectHandler<T>(info: CacheInfo, keyPrefix: String) : BaseCacheHandler<T>(info, keyPrefix) {
+
     override fun encode(value: T, clazz: Class<T>?): ByteArray {
         if (clazz == null) throw CacheException("class is null")
-        return cacheInfo.objectConverter.encode(value, clazz)
+        return cacheInfo.objectConverter.encode(value, clazz).also {
+            if (it.isEmpty()) error("Converter encode return empty ${clazz.name}")
+        }
     }
 
     override fun decode(bytes: ByteArray, clazz: Class<T>?): T? {
