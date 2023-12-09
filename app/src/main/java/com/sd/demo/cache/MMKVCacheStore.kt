38fa57391b2d88816e6024231fb3e4c88c06caea
zjun <565061763@qq.com>
2023-12-09 21:23:06 +0800

    mmkv

--- app/src/main/java/com/sd/demo/cache/MMKVCacheStore.kt
+++ app/src/main/java/com/sd/demo/cache/MMKVCacheStore.kt
@@ -9,7 +9,7 @@ import com.tencent.mmkv.MMKV
  *
  * MMKV有个bug，如果存的是空byte[]，取的时候会返回null，正常返回应该也是个空byte[]
  */
-class MMKVCacheStore(context: Context?) : CacheStore {
+class MMKVCacheStore(context: Context) : CacheStore {
     private val _mmkv: MMKV
 
     init {
