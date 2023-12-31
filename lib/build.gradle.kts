c10124c05aad0074fe03a81c58464156b90f654a
zjun <565061763@qq.com>
2023-12-31 21:24:14 +0800

    2.0.0-beta01

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "cache"
-val libVersionName = "2.0.0-alpha02"
+val libVersionName = "2.0.0-beta01"
 
 android {
     namespace = "com.sd.lib.cache"
