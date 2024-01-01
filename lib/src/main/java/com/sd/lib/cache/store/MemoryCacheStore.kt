8b343e9d7b2e05757e2407f7b9a0f475c70e6ac7
zjun <565061763@qq.com>
2024-01-01 22:14:12 +0800

    MemoryCacheStore

---
+++ lib/src/main/java/com/sd/lib/cache/store/MemoryCacheStore.kt
@@ -0,0 +1,35 @@
+package com.sd.lib.cache.store
+
+import android.content.Context
+import java.io.File
+
+/**
+ * 保存在内存中的仓库
+ */
+internal class MemoryCacheStore : CacheStore {
+    private val _holder: MutableMap<String, ByteArray> = hashMapOf()
+
+    override fun init(context: Context, directory: File, id: String) {
+    }
+
+    override fun putCache(key: String, value: ByteArray): Boolean {
+        _holder[key] = value
+        return true
+    }
+
+    override fun getCache(key: String): ByteArray? {
+        return _holder[key]
+    }
+
+    override fun removeCache(key: String) {
+        _holder.remove(key)
+    }
+
+    override fun containsCache(key: String): Boolean {
+        return _holder.containsKey(key)
+    }
+
+    override fun keys(): Array<String>? {
+        return _holder.keys.toTypedArray()
+    }
+}
