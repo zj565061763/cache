df391a2ca0d6dacb1f716f61f1bcc0cf038c605b
zjun <565061763@qq.com>
2024-01-31 00:00:57 +0800

    demo

--- app/src/main/java/com/sd/demo/cache/SamplePerformance.kt
+++ app/src/main/java/com/sd/demo/cache/SamplePerformance.kt
@@ -1,10 +1,12 @@
 package com.sd.demo.cache
 
 import android.os.Bundle
+import android.util.Base64
 import androidx.appcompat.app.AppCompatActivity
 import com.sd.demo.cache.databinding.SamplePerformanceBinding
 import com.sd.lib.cache.Cache
 import com.sd.lib.cache.FCache
+import java.security.MessageDigest
 import kotlin.time.measureTime
 
 class SamplePerformance : AppCompatActivity() {
@@ -45,6 +47,34 @@ private fun testPerformanceGet(
     measureTime {
         repeat(repeat) { index ->
             cache.cString().get(index.toString())
+        }
+    }.let {
+        logMsg { "get time:${it.inWholeMilliseconds}" }
+    }
+}
+
+private fun testMd5(
+    repeat: Int = 10000,
+) {
+    val input = "1".repeat(50).toByteArray()
+    measureTime {
+        repeat(repeat) {
+            MessageDigest.getInstance("MD5")
+                .digest(input)
+                .joinToString("") { "%02X".format(it) }
+        }
+    }.let {
+        logMsg { "get time:${it.inWholeMilliseconds}" }
+    }
+}
+
+private fun testBase64(
+    repeat: Int = 10000,
+) {
+    val input = "1".repeat(50).toByteArray()
+    measureTime {
+        repeat(repeat) {
+            Base64.encode(input, Base64.DEFAULT).decodeToString()
         }
     }.let {
         logMsg { "get time:${it.inWholeMilliseconds}" }
