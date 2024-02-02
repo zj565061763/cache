49a79557c25affcde0aa03217ed3b788ce018864
zjun <565061763@qq.com>
2024-02-02 23:50:08 +0800

    comment

--- app/src/main/java/com/sd/demo/cache/App.kt
+++ app/src/main/java/com/sd/demo/cache/App.kt
@@ -2,6 +2,7 @@ package com.sd.demo.cache
 
 import android.app.Application
 import com.sd.demo.cache.impl.CacheExceptionHandler
+import com.sd.demo.cache.impl.MoshiObjectConverter
 import com.sd.lib.cache.CacheConfig
 
 class App : Application() {
@@ -9,9 +10,12 @@ class App : Application() {
         super.onCreate()
         CacheConfig.init(
             CacheConfig.Builder()
+                // 设置缓存目录
                 .setDirectory(getExternalFilesDir("app_cache")!!)
+                // 设置异常处理
                 .setExceptionHandler(CacheExceptionHandler())
-//                .setObjectConverter(MoshiObjectConverter())
+                // 设置对象转换器，默认为Gson转换器
+                .setObjectConverter(MoshiObjectConverter())
                 .build(this)
         )
     }
