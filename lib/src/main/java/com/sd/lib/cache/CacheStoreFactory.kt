3be6f7768c00e1b8ea91325a02165d9421d24835
zjun <565061763@qq.com>
2024-03-16 22:19:24 +0800

    format

--- lib/src/main/java/com/sd/lib/cache/CacheStoreFactory.kt
+++ lib/src/main/java/com/sd/lib/cache/CacheStoreFactory.kt
@@ -3,7 +3,7 @@ package com.sd.lib.cache
 import com.sd.lib.cache.store.CacheStore
 
 internal class CacheStoreFactory(
-    private val group: String,
+    private val group: String
 ) {
     private var _isClosed = false
     private val _stores: MutableMap<String, StoreInfo> = hashMapOf()
