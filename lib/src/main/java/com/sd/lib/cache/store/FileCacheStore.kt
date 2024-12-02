d38eb2e42b46ef6fad7826844002f250c4618ccf
zjun <565061763@qq.com>
2024-12-03 03:01:35 +0800

    FileCacheStore

--- lib/src/main/java/com/sd/lib/cache/store/FileCacheStore.kt
+++ lib/src/main/java/com/sd/lib/cache/store/FileCacheStore.kt
@@ -4,14 +4,14 @@ import java.io.File
 import java.io.FileNotFoundException
 
 internal class FileCacheStore : DirectoryCacheStore() {
-
     override fun putCacheImpl(file: File, value: ByteArray): Boolean {
         return try {
             file.writeBytes(value)
             true
         } catch (e: FileNotFoundException) {
             if (directory.fMakeDirs()) {
-                writeFile(file, value)
+                file.writeBytes(value)
+                true
             } else {
                 throw e
             }
@@ -27,24 +27,8 @@ internal class FileCacheStore : DirectoryCacheStore() {
     }
 }
 
-private fun File?.fMakeDirs(): Boolean {
-    if (this == null) return false
-    if (this.isDirectory) return true
-    if (this.isFile) this.delete()
-    return this.mkdirs()
-}
-
-private fun writeFile(file: File, value: ByteArray): Boolean {
-    return try {
-        file.writeBytes(value)
-        true
-    } catch (e: FileNotFoundException) {
-        if (file.isDirectory) {
-            file.deleteRecursively()
-            file.writeBytes(value)
-            true
-        } else {
-            throw e
-        }
-    }
+private fun File.fMakeDirs(): Boolean {
+    if (isDirectory) return true
+    if (isFile) delete()
+    return mkdirs()
 }
