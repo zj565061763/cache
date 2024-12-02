b08dbcd1caf83797b3781dc2d59b3cf41ff4742b
zjun <565061763@qq.com>
2024-12-03 03:33:17 +0800

    style

--- lib/src/main/java/com/sd/lib/cache/handler/ObjectHandler.kt
+++ lib/src/main/java/com/sd/lib/cache/handler/ObjectHandler.kt
@@ -2,16 +2,14 @@ package com.sd.lib.cache.handler
 
 import com.sd.lib.cache.CacheException
 
-/**
- * Object处理类
- */
 internal class ObjectHandler<T>(info: CacheInfo, handlerKey: String) : BaseCacheHandler<T>(info, handlerKey) {
     override fun encode(value: T, clazz: Class<T>): ByteArray {
-        return cacheInfo.objectConverter.encode(value, clazz).also {
-            if (it.isEmpty()) {
-                throw CacheException("Converter encode returns empty ${clazz.name}")
+        return cacheInfo.objectConverter.encode(value, clazz)
+            .also {
+                if (it.isEmpty()) {
+                    throw CacheException("Converter encode returns empty ${clazz.name}")
+                }
             }
-        }
     }
 
     override fun decode(bytes: ByteArray, clazz: Class<T>): T? {
