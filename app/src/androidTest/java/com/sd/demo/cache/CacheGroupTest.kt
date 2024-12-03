acfd68d97d23c8970fcfc70b88547ec1bfb01a8c
zjun <565061763@qq.com>
2024-12-03 14:28:59 +0800

    style

--- app/src/androidTest/java/com/sd/demo/cache/CacheGroupTest.kt
+++ app/src/androidTest/java/com/sd/demo/cache/CacheGroupTest.kt
@@ -3,7 +3,7 @@ package com.sd.demo.cache
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.sd.lib.cache.Cache
 import com.sd.lib.cache.FCache
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -25,15 +25,16 @@ class CacheGroupTest {
 
 private fun testCacheEmptyActiveGroup(cache: Cache) {
     val key = "testCacheEmptyActiveGroup"
+
     val c = cache.multi(TestModel::class.java)
+    c.remove(key)
 
     // test get defaultValue
-    c.remove(key)
-    Assert.assertEquals(false, c.contains(key))
-    Assert.assertEquals(null, c.get(key))
+    assertEquals(false, c.contains(key))
+    assertEquals(null, c.get(key))
 
     // test put and get
-    Assert.assertEquals(false, c.put(key, TestModel()))
-    Assert.assertEquals(false, c.contains(key))
-    Assert.assertEquals(null, c.get(key))
+    assertEquals(false, c.put(key, TestModel()))
+    assertEquals(false, c.contains(key))
+    assertEquals(null, c.get(key))
 }
