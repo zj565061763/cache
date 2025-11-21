b042662577dbd55d5e7d2ca3750ac8fcfa80bc11
zjun <565061763@qq.com>
2025-11-21 10:00:29 +0800

    refactor

--- lib/src/main/java/com/sd/lib/cache/CacheConfig.kt
+++ lib/src/main/java/com/sd/lib/cache/CacheConfig.kt
@@ -1,10 +1,6 @@
 package com.sd.lib.cache
 
-import android.app.ActivityManager
-import android.app.Application
 import android.content.Context
-import android.os.Build
-import android.os.Process
 import com.sd.lib.cache.store.CacheStore
 import com.sd.lib.cache.store.FileCacheStore
 import java.io.File
@@ -139,26 +135,4 @@ inline fun CacheConfig.Companion.init(
       .apply(block)
       .build(context)
   )
-}
-
-private fun Context.defaultCacheDir(): File {
-  val dir = filesDir.resolve("sd.lib.cache")
-  val process = currentProcess()
-  return if (process != packageName && !process.isNullOrBlank()) {
-    dir.resolve(process)
-  } else {
-    dir
-  }
-}
-
-private fun Context.currentProcess(): String? {
-  return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-    Application.getProcessName()
-  } else {
-    val pid = Process.myPid()
-    (getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager)
-      ?.runningAppProcesses
-      ?.firstOrNull { it.pid == pid }
-      ?.processName
-  }
 }
