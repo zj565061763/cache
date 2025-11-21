84fb511e9121002166444a7b6a5effa575e97b5b
zjun <565061763@qq.com>
2025-11-21 09:41:27 +0800

    comment

--- lib/src/main/java/com/sd/lib/cache/store/CacheStore.kt
+++ lib/src/main/java/com/sd/lib/cache/store/CacheStore.kt
@@ -7,9 +7,7 @@ import java.io.File
  * 缓存仓库
  */
 interface CacheStore {
-  /**
-   * 初始化
-   */
+  /** 初始化 */
   @Throws(Throwable::class)
   fun init(
     context: Context,
@@ -18,39 +16,27 @@ interface CacheStore {
     id: String,
   )
 
-  /**
-   * 设置缓存
-   */
+  /** 设置缓存 */
   @Throws(Throwable::class)
   fun putCache(key: String, value: ByteArray)
 
-  /**
-   * 获取缓存
-   */
+  /** 获取缓存 */
   @Throws(Throwable::class)
   fun getCache(key: String): ByteArray?
 
-  /**
-   * 删除缓存
-   */
+  /** 删除缓存 */
   @Throws(Throwable::class)
   fun removeCache(key: String)
 
-  /**
-   * 是否有[key]对应的缓存
-   */
+  /** 是否有[key]对应的缓存 */
   @Throws(Throwable::class)
   fun containsCache(key: String): Boolean
 
-  /**
-   * 返回所有的Key
-   */
+  /** 返回所有的Key */
   @Throws(Throwable::class)
   fun keys(): List<String>
 
-  /**
-   * 关闭
-   */
+  /** 关闭 */
   @Throws(Throwable::class)
   fun close()
 }
