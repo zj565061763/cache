5709813f501cd5b09fe4fab36d2928afada53471
zjun <565061763@qq.com>
2025-11-28 17:44:34 +0800

    sample

--- app/src/main/java/com/sd/demo/cache/SampleActiveGroup.kt
+++ app/src/main/java/com/sd/demo/cache/SampleActiveGroup.kt
@@ -10,8 +10,8 @@ class SampleActiveGroup : AppCompatActivity() {
 
   private val _cache = FCache.get(ActiveModel::class.java)
 
-  private val key1 = "key1"
-  private val key2 = "key2"
+  private val _key1 = "key1"
+  private val _key2 = "key2"
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -43,27 +43,27 @@ class SampleActiveGroup : AppCompatActivity() {
 
   private fun putCache() {
     val model = ActiveModel()
-    _cache.put(key1, model).also { logMsg { "put key1:$it" } }
-    _cache.put(key2, model).also { logMsg { "put key2:$it" } }
+    _cache.put(_key1, model).also { logMsg { "put key1:$it" } }
+    _cache.put(_key2, model).also { logMsg { "put key2:$it" } }
   }
 
   private fun getCache() {
-    _cache.get(key1).also { logMsg { "get key1:$it" } }
-    _cache.get(key2).also { logMsg { "get key2:$it" } }
+    _cache.get(_key1).also { logMsg { "get key1:$it" } }
+    _cache.get(_key2).also { logMsg { "get key2:$it" } }
     _cache.keys().also { keys ->
       logMsg { "keys:" + keys.joinToString(prefix = "[", postfix = "]", separator = ",") }
     }
   }
 
   private fun containsCache() {
-    _cache.contains(key1).also { logMsg { "contains key1:$it" } }
-    _cache.contains(key2).also { logMsg { "contains key2:$it" } }
+    _cache.contains(_key1).also { logMsg { "contains key1:$it" } }
+    _cache.contains(_key2).also { logMsg { "contains key2:$it" } }
   }
 
   private fun removeCache() {
     logMsg { "removeCache" }
-    _cache.remove(key1)
-    _cache.remove(key2)
+    _cache.remove(_key1)
+    _cache.remove(_key2)
   }
 
   override fun onDestroy() {
--- app/src/main/java/com/sd/demo/cache/SamplePerformance.kt
+++ app/src/main/java/com/sd/demo/cache/SamplePerformance.kt
@@ -9,6 +9,10 @@ import kotlin.time.measureTime
 class SamplePerformance : AppCompatActivity() {
   private val _binding by lazy { SamplePerformanceBinding.inflate(layoutInflater) }
 
+  private val _cache = FCache.get(DefaultModel::class.java)
+
+  private val _key = "performance"
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(_binding.root)
@@ -19,29 +23,26 @@ class SamplePerformance : AppCompatActivity() {
       testGet()
     }
   }
-}
 
-private fun testPut(repeat: Int = 100) {
-  val cache = FCache.get(DefaultModel::class.java)
-  val key = "key"
-  val model = DefaultModel()
-  measureTime {
-    repeat(repeat) {
-      cache.put(key, model)
+  private fun testPut(repeat: Int = 100) {
+    val model = DefaultModel()
+    measureTime {
+      repeat(repeat) {
+        _cache.put(_key, model)
+      }
+    }.also {
+      logMsg { "put time:${it.inWholeMilliseconds}" }
     }
-  }.also {
-    logMsg { "put time:${it.inWholeMilliseconds}" }
   }
-}
 
-private fun testGet(repeat: Int = 100) {
-  val cache = FCache.get(DefaultModel::class.java)
-  val key = "key"
-  measureTime {
-    repeat(repeat) {
-      cache.get(key)
+  private fun testGet(repeat: Int = 100) {
+    val key = "key"
+    measureTime {
+      repeat(repeat) {
+        _cache.get(key)
+      }
+    }.also {
+      logMsg { "get time:${it.inWholeMilliseconds}" }
     }
-  }.also {
-    logMsg { "get time:${it.inWholeMilliseconds}" }
   }
 }
