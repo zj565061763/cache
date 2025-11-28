d2858801b177ff71e18f2990e14ec225b699bba0
zjun <565061763@qq.com>
2025-11-28 18:05:07 +0800

    sample

--- app/src/main/java/com/sd/demo/cache/App.kt
+++ app/src/main/java/com/sd/demo/cache/App.kt
@@ -2,7 +2,10 @@ package com.sd.demo.cache
 
 import android.app.Application
 import com.sd.lib.cache.CacheConfig
+import com.sd.lib.cache.FCacheKtx
 import com.sd.lib.cache.init
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class App : Application() {
   override fun onCreate() {
@@ -12,6 +15,14 @@ class App : Application() {
       setExceptionHandler { error ->
         logMsg { "error:${error}" }
       }
+    }
+
+    GlobalScope.launch {
+      FCacheKtx.get(DefaultModel::class.java)
+        .flowOfKeys()
+        .collect { keys ->
+          logMsg { "App keys:$keys" }
+        }
     }
   }
 }
