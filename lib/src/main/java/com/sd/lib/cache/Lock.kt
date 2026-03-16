8a00afb9d6a382cca63886d751e2b766fdc4288f
zjun <565061763@qq.com>
2026-03-16 09:57:50 +0800

    多进程锁使用LocalServerSocket

--- lib/src/main/java/com/sd/lib/cache/Lock.kt
+++ lib/src/main/java/com/sd/lib/cache/Lock.kt
@@ -1,40 +1,39 @@
 package com.sd.lib.cache
 
-import java.io.File
-import java.io.RandomAccessFile
+import android.net.LocalServerSocket
+import java.io.IOException
 
 internal fun <T> libLock(block: () -> T): T {
   return if (CacheConfig.get().multiProcessEnabled) {
-    fileProcessLock.lock(block)
+    socketProcessLock.lock(block)
   } else {
     synchronized(FCache) { block() }
   }
 }
 
-private val fileProcessLock by lazy {
-  FileProcessLock(
-    lockFile = CacheConfig.get().directory.resolve("cache.lock"),
+private val socketProcessLock by lazy {
+  SocketProcessLock(
+    name = "com.sd.lib.cache:${CacheConfig.get().context.packageName}",
     currentProcessLock = FCache,
   )
 }
 
-private class FileProcessLock(
-  private val lockFile: File,
+private class SocketProcessLock(
+  private val name: String,
   private val currentProcessLock: Any,
 ) {
-  private var _raf: RandomAccessFile? = null
+  private var _socket: LocalServerSocket? = null
 
   fun <T> lock(block: () -> T): T {
     synchronized(currentProcessLock) {
-      if (_raf == null) {
-        val raf = RandomAccessFile(lockFile, "rw")
+      if (_socket == null) {
+        val socket = realLock()
         return try {
-          _raf = raf
-          raf.channel.lock()
+          _socket = socket
           block()
         } finally {
-          runCatching { raf.close() }
-          _raf = null
+          runCatching { socket.close() }
+          _socket = null
         }
       } else {
         return block()
@@ -42,7 +41,21 @@ private class FileProcessLock(
     }
   }
 
-  init {
-    lockFile.parentFile?.mkdirs()
+  private fun realLock(): LocalServerSocket {
+    var retryCount = 0
+    while (true) {
+      try {
+        return LocalServerSocket(name)
+      } catch (e: IOException) {
+        if (retryCount >= 1000) throw IOException("Get multi-process lock timeout", e)
+        retryCount++
+        try {
+          Thread.sleep(10)
+        } catch (ie: InterruptedException) {
+          Thread.currentThread().interrupt()
+          throw ie
+        }
+      }
+    }
   }
 }
