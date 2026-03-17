dd2c5739cf3c9f056a047bb995447d59770d5bb2
zjun <565061763@qq.com>
2026-03-17 18:52:03 +0800

    style

--- lib/src/main/java/com/sd/lib/cache/Cache.kt
+++ lib/src/main/java/com/sd/lib/cache/Cache.kt
@@ -1,5 +1,7 @@
 package com.sd.lib.cache
 
+import com.sd.lib.cache.store.CacheStore
+
 interface Cache<T> {
   /** 设置缓存 */
   fun put(key: String, value: T?): Boolean
@@ -15,4 +17,84 @@ interface Cache<T> {
 
   /** 所有缓存key */
   fun keys(): List<String>
+}
+
+internal class CacheImpl<T>(
+  private val clazz: Class<T>,
+  private val cacheStoreProvider: () -> CacheStore,
+) : Cache<T> {
+
+  var cacheChangeCallback: CacheStore.CacheChangeCallback? = null
+
+  override fun put(key: String, value: T?): Boolean {
+    if (value == null) return false
+    return libRunCatching {
+      val data = encode(value, clazz)
+      libLock { getCacheStore().putCache(key, data) }
+      true
+    }.getOrElse { false }
+  }
+
+  override fun get(key: String): T? {
+    return libRunCatching {
+      libLock { getCacheStore().getCache(key) }
+        ?.let { data -> decode(data, clazz) }
+    }.getOrNull()
+  }
+
+  override fun remove(key: String) {
+    libRunCatching {
+      libLock { getCacheStore().removeCache(key) }
+    }
+  }
+
+  override fun contains(key: String): Boolean {
+    return libRunCatching {
+      libLock { getCacheStore().containsCache(key) }
+    }.getOrElse { false }
+  }
+
+  override fun keys(): List<String> {
+    return libRunCatching {
+      libLock { getCacheStore().keys() }
+    }.getOrElse { emptyList() }
+  }
+
+  @Throws(Throwable::class)
+  private fun getCacheStore(): CacheStore {
+    return cacheStoreProvider().also { cacheStore ->
+      cacheStore.setCacheChangeCallback(_cacheChangeCallback)
+    }
+  }
+
+  @Throws(Throwable::class)
+  private fun encode(value: T, clazz: Class<T>): ByteArray {
+    return getObjectConverter().encode(value, clazz).also { bytes ->
+      if (bytes.isEmpty()) {
+        libException("ObjectConverter.encode returns empty ${clazz.name}")
+      }
+    }
+  }
+
+  @Throws(Throwable::class)
+  private fun decode(bytes: ByteArray, clazz: Class<T>): T? {
+    if (bytes.isEmpty()) return null
+    return getObjectConverter().decode(bytes, clazz)
+  }
+
+  private fun getObjectConverter(): CacheConfig.ObjectConverter = CacheConfig.get().objectConverter
+
+  private val _cacheChangeCallback = object : CacheStore.CacheChangeCallback {
+    override fun onRemove(key: String) {
+      cacheChangeCallback?.onRemove(key)
+    }
+
+    override fun onModify(key: String) {
+      cacheChangeCallback?.onModify(key)
+    }
+  }
+
+  init {
+    getCacheStore()
+  }
 }
