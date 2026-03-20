003523647f380e52b1f826e22b73727486419e66
zjun <565061763@qq.com>
2026-03-20 13:58:08 +0800

    update返回值

--- lib/src/main/java/com/sd/lib/cache/SingleCacheKtx.kt
+++ lib/src/main/java/com/sd/lib/cache/SingleCacheKtx.kt
@@ -10,43 +10,47 @@ interface SingleCacheKtx<T> {
   /** 缓存 */
   fun flow(): Flow<T>
 
-  /** 更新缓存，[block]在[Dispatchers.IO]上面执行，如果[block]返回null则删除缓存 */
-  suspend fun update(block: suspend (T) -> T?)
+  /**
+   * 更新缓存，[block]在[Dispatchers.IO]上面执行，如果[block]返回null则删除缓存
+   * @return true-更新成功；false-更新失败
+   */
+  suspend fun update(block: suspend (T) -> T?): Boolean
 }
 
 fun <T> CacheKtx<T>.asSingleCacheKtx(
   /** 缓存key */
   key: String = FCache.DEFAULT_SINGLE_CACHE_KEY,
   /** 默认缓存，在[Dispatchers.IO]上面执行 */
-  defaultCache: () -> T,
+  getDefault: () -> T,
 ): SingleCacheKtx<T> {
   return SingleCacheKtxImpl(
     cache = this,
     key = key,
-    defaultCache = defaultCache,
+    getDefault = getDefault,
   )
 }
 
 private class SingleCacheKtxImpl<T>(
   private val cache: CacheKtx<T>,
   private val key: String,
-  private val defaultCache: () -> T,
+  private val getDefault: () -> T,
 ) : SingleCacheKtx<T> {
   private val _flow = cache.flowOf(key)
-    .map { it ?: defaultCache() }
+    .map { it ?: getDefault() }
     .distinctUntilChanged()
     .flowOn(Dispatchers.IO)
 
   override fun flow(): Flow<T> = _flow
 
-  override suspend fun update(block: suspend (T) -> T?) {
-    cache.edit {
-      val oldCache = get(key) ?: defaultCache()
+  override suspend fun update(block: suspend (T) -> T?): Boolean {
+    return cache.edit {
+      val oldCache = get(key) ?: getDefault()
       val newCache = block(oldCache)
       if (newCache != null) {
         put(key, newCache)
       } else {
         remove(key)
+        true
       }
     }
   }
