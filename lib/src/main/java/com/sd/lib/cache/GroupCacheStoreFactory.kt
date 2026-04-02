4e927dd90761c19374817bc6dd4d9f2e21c44a44
zjun <565061763@qq.com>
2026-04-02 12:20:01 +0800

    GroupCacheStoreFactory

--- lib/src/main/java/com/sd/lib/cache/GroupCacheStoreFactory.kt
+++ lib/src/main/java/com/sd/lib/cache/GroupCacheStoreFactory.kt
@@ -1,17 +1,15 @@
 package com.sd.lib.cache
 
 import com.sd.lib.cache.store.CacheStore
+import java.util.concurrent.ConcurrentHashMap
 
 internal class GroupCacheStoreFactory(
   val group: String,
 ) {
-  private var _destroyed = false
-  private val _stores = mutableMapOf<String, StoreInfo>()
+  private val _stores: MutableMap<String, StoreInfo> = ConcurrentHashMap()
 
   @Throws(Throwable::class)
   fun create(id: String, clazz: Class<*>): CacheStore {
-    if (_destroyed) libError("Group:${group} has been destroyed")
-
     _stores[id]?.also { info ->
       if (info.clazz == clazz) {
         return info.cacheStore
@@ -19,15 +17,17 @@ internal class GroupCacheStoreFactory(
         libError("id:${id} has bound to ${info.clazz.name} when bind ${clazz.name}")
       }
     }
-
-    return CacheConfig.get().newCacheStore(group = group, id = id)
-      .also { cacheStore -> _stores[id] = StoreInfo(clazz, cacheStore) }
-  }
-
-  fun destroy() {
-    _destroyed = true
-    _stores.values.forEach { it.cacheStore.destroyQuietly() }
-    _stores.clear()
+    synchronized(this@GroupCacheStoreFactory) {
+      _stores[id]?.also { info ->
+        if (info.clazz == clazz) {
+          return info.cacheStore
+        } else {
+          libError("id:${id} has bound to ${info.clazz.name} when bind ${clazz.name}")
+        }
+      }
+      return CacheConfig.get().newCacheStore(group = group, id = id)
+        .also { cacheStore -> _stores[id] = StoreInfo(clazz, cacheStore) }
+    }
   }
 
   private class StoreInfo(
@@ -35,5 +35,3 @@ internal class GroupCacheStoreFactory(
     val cacheStore: CacheStore,
   )
 }
-
-private fun CacheStore.destroyQuietly() = libRunCatching { destroy() }
